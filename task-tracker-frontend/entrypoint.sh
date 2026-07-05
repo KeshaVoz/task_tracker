f92e597aa566d9775c90ad22e83ffe40e27e5d6b
Kesha <kesha.voznesensky@gmail.com>
2026-07-05 19:40:06 +0300

    Frontend minor improvement

--- task-tracker-frontend/entrypoint.sh
+++ task-tracker-frontend/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sed -i "s|http://localhost:8080/api|${API_URL}|g" /usr/share/nginx/html/script.js
+cp /usr/share/nginx/html/nginx.conf /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
